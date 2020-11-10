b6a02f73cba70b89e7707e9be2e27aff10e87eb1
Koyoungsung <ken3650@naver.com>
2020-11-10 18:37:12 +0900

    프로젝트 세팅 시큐리티 연동/설정

---
+++ inflearn-spring-security/src/test/java/ko/inflearnspringsecurity/InflearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ko.inflearnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
