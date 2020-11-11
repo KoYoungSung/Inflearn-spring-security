47362263eafd7b5276b38b3a0bcd48f449bc2154
Koyoungsung <ken3650@naver.com>
2020-11-11 19:32:45 +0900

    JPA 연동

---
+++ inflearn-spring-security/src/main/java/ko/inflearnspringsecurity/account/AccountController.java
@@ -0,0 +1,21 @@
+package ko.inflearnspringsecurity.account;
+
+// 편의상 만드는 것으로 실제에서 하면 안됨
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @Autowired
+    AccountService accountService;
+
+    @GetMapping("/account/{role}/{username}/{password}")
+    public Account createAccount(@ModelAttribute Account account) {
+        return accountService.createNew(account);
+    }
+}
