6fcb382e0e4bafb753b2b4587cc0cd45e45a0a7f
user-PC\user <ko>
2020-11-12 19:28:16 +0900

    스프링 시큐리티 필터와 FilterChainProxy

--- inflearn-spring-security/src/main/java/ko/inflearnspringsecurity/config/SecurityConfig.java
+++ inflearn-spring-security/src/main/java/ko/inflearnspringsecurity/config/SecurityConfig.java
@@ -1,25 +1,26 @@
 package ko.inflearnspringsecurity.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @ConfigurationProperties
-@EnableWebSecurity
+@Order(Ordered.LOWEST_PRECEDENCE - 100)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", "/info","/account/**").permitAll()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic();
+                .anyRequest().authenticated();
+
+        http.formLogin();
+        http.httpBasic();
     }
 
 
