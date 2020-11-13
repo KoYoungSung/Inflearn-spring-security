f7dab631ccc3ad3aac5ea0d5bfd9e95a3bfdee1c
user-PC\user <ko>
2020-11-13 17:55:50 +0900

    DefaultLogin/LogoutPageGeneratingFilter 로그인/로그아웃 폼 커스터마이징

--- inflearn-spring-security/src/main/java/ko/inflearnspringsecurity/config/SecurityConfig.java
+++ inflearn-spring-security/src/main/java/ko/inflearnspringsecurity/config/SecurityConfig.java
@@ -39,7 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
                 .expressionHandler(expressionHandler());
-        http.formLogin();
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
         http.httpBasic();
 
         http.logout().logoutSuccessUrl("/");
